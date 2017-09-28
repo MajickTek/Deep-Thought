c7e6f4c12050a9a0547da770578ed48fc5bcc9c5
Majick Tek <kingtmw2@gmail.com>
2017-09-28 10:54:14 -0400

    Add Deep Though Algorithm

---
+++ main.c
@@ -0,0 +1,17 @@
+//DEEP THOUGHT ULTIMATE QUESTION TO LIFE, THE UNIVERSE, AND EVERYTHING
+//this took a very long time to generate
+//THE ANSWER IS 42.
+//the question was confirmed long afterwards to be "what is 6 times 9".
+#include <stdio.h> // standard procedures
+
+#define SIX 1+5 // 1 + 5 is 6
+#define NINE 8+1 // 8 + 1 is 9
+
+int main(void) // this function holds the Deep Thought Algorithm
+{
+    printf("<:Deep Thought:> GENERATING SHORTENED ALGORITHM\n");
+    printf("<:ECHO:Deep Thought:>Here is The Question and The Answer\n");
+    printf("%d * %d = %d\n", SIX, NINE, SIX * NINE); // The Algorithm, 6*9=42
+    return 0; // If this was not here, the program would literally run for infinity,
+    //and we would never know the Answer OR the Question
+}
